3083a329d4decefd98d03f1fd113ec3cccc84c1e
Spencer <spengy@gmail.com>
2013-01-29 15:08:34 -0700

    Added some comments to the code

--- QKDTree/QKDTree.h
+++ QKDTree/QKDTree.h
@@ -10,10 +10,24 @@
 class QKDTREESHARED_EXPORT QKDTree
 {
 public:
+    /**
+     * @brief QKDTree constructs a kd-tree that takes positions of the given dimension.
+     * e.g., to store 2d points, set dimension = 2.
+     * @param dimension
+     */
     QKDTree(uint dimension);
     ~QKDTree();
 
+    /**
+     * @brief dimension Returns the dimensionality of the positions stored by this tree.
+     * @return
+     */
     uint dimension() const;
+
+    /**
+     * @brief size Returns the number of key/value pairs in the tree
+     * @return
+     */
     qint64 size() const;
 
     bool add(QKDTreeNode * node, QString * resultOut = 0);
@@ -29,6 +43,9 @@ public:
     bool setDistanceMetric(QKDTreeDistanceMetric * nMetric, QString * resultOut = 0);
     QKDTreeDistanceMetric * distanceMetric() const;
 
+    /**
+     * @brief debugPrint does a breadth-first search of the tree, printing values as it goes.
+     */
     void debugPrint();
 
 private:
--- QKDTree/QKDTreeDistanceMetric.h
+++ QKDTree/QKDTreeDistanceMetric.h
@@ -3,6 +3,10 @@
 
 #include "QKDTreeNode.h"
 
+/**
+ * @brief The QKDTreeDistanceMetric class is used by QKDTree to calculate distances between positions.
+ * This base implementation returns squared euclidean distance.
+ */
 class QKDTreeDistanceMetric
 {
 public:
@@ -10,6 +14,15 @@ public:
     virtual ~QKDTreeDistanceMetric();
 
     qreal distance(const QKDTreeNode * const a, const QKDTreeNode * const b);
+
+    /**
+     * @brief distance This method returns the distance between two positions in the tree.
+     * To use a custom distance metric, create an inheriting class that overrides this method and
+     * pass the child class to your tree using QKDTree::setDistanceMetric
+     * @param a
+     * @param b
+     * @return
+     */
     virtual qreal distance(const QVectorND& a, const QVectorND& b);
 };
 
